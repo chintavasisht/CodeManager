12ca138e8f7a2d2c76727bc9c26e523552baeb32
vasisht <vasisht@VSENT>
2016-11-03 09:14:15 +0000

    Add project files.

---
+++ CodeManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeManager.Startup))]
+namespace CodeManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
